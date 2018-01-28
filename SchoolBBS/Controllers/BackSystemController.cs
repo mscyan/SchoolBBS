248d5decd3596acbb407264600b532e782ca0b64
mscyan <yanchao00001@hotmail.com>
2018-01-28 22:43:52 +0800

    后台前端框架

---
+++ SchoolBBS/Controllers/BackSystemController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolBBS.Controllers
+{
+    public class BackSystemController : Controller
+    {
+        // GET: BackSystem
+        public ActionResult BackSystemIndex()
+        {
+            return View();
+        }
+
+		//社区管理界面
+		public ActionResult CommunityManage()
+		{
+			return View();
+		}
+
+		//帖子管理界面
+		public ActionResult PostManage()
+		{
+			return View();
+		}
+
+		//回复管理界面
+		public ActionResult ReplyManage()
+		{
+			return View();
+		} 
+
+		//用户管理界面
+		public ActionResult UserManege()
+		{
+			return View();
+		}
+    }
+}
